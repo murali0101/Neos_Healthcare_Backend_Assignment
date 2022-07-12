7d9fd1d338e1ef5505f85d112a0a3e28c1046320
murali0101 <muralimv0101@gmail.com>
2022-07-13 01:04:44 +0530

    welcom note

--- src/server.js
+++ src/server.js
@@ -6,7 +6,10 @@ const {
   deleteUser,
 } = require("./controllers/userController");
 const server = http.createServer((req, res) => {
-  if (req.url === "/signup" && req.method === "GET") {
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: `welcome to "neos-healthcare" backend (server)` }))
+  } else if (req.url === "/signup" && req.method === "GET") {
     getUsers(req, res);
   } else if (req.url === "/signup" && req.method === "POST") {
     createUser(req, res);
