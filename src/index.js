c017c61480d9c4e8fc87f83d1615ec0b1a6deb66
murali0101 <muralimv0101@gmail.com>
2022-07-12 15:33:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-const http = require('node:http');
+const http = require("node:http");
 
 // Create a local server to receive data from
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    data: 'Hello World!'
-  }));
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: "Hello World!",
+    })
+  );
 });
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 server.listen(port, async (req, res) => {
-    console.log(`listening port ${port} .......`)
+  console.log(`listening port ${port} .......`);
 });
+
+
+
